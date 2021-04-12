be21473542a5373c7e8f01b96ff6ea5c666e8961
Bruna Barreto <brunatrebienbarreto@outlook.com>
2021-04-12 17:37:40 -0300

    Project created

---
+++ backend/src/main/java/com/capdesenvolvimento/aulaDev/AulaDevApplication.java
@@ -0,0 +1,13 @@
+package com.capdesenvolvimento.aulaDev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaDevApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/capdesenvolvimento/aulaDev/AulaDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capdesenvolvimento.aulaDev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
